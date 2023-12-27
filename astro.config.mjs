b5da706c8153945ca028baaa0dea260e3e8d2ea8
Seth Horsley <isethi@me.com>
2023-12-27 11:33:43 +0100

    add tailwind and shadcn and add a bit of style to homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,36 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import XMLLoader from "./xml-loader";
 import nodejs from "@astrojs/node";
-
 import compress from "astro-compress";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), compress()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    compress(),
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    react(),
+  ],
   output: "hybrid",
   server: {
-    host: "0.0.0.0",
+    host: "0.0.0.0"
   },
   adapter: nodejs({
-    mode: "standalone",
+    mode: "standalone"
   }),
   vite: {
-    plugins: [
-      XMLLoader({
-        ignoreAttributes: false,
-      }),
-    ],
-  },
+    plugins: [XMLLoader({
+      ignoreAttributes: false
+    })],
+    ssr: {
+      noExternal: ["@radix-ui/*"],
+    }
+  }
 });
